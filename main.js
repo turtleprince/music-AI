42eb957f0f09ca2b55f1d0b7106f268f7f061605
turtleprince <81336142+turtleprince@users.noreply.github.com>
2022-07-22 20:00:09 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 var song = "";
+var leftWristScore = "";
+var rightWristScore = "";
+var leftWrist = "";
+var rightWrist = "";
 
 
 function preload(){
-    song = loadSound("music2.mp3");
+    song = loadSound("music.mp3");
     
 }
 
@@ -12,6 +16,38 @@ function setup(){
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelloaded);
+    poseNet.on('pose', got_results);
+}
+
+
+function modelloaded() {
+    console.log("modelloaded");
+}
+
+
+function got_results(results) {
+
+    if (results.length > 0) {
+        //console.log(results);
+
+        leftWrist_Score = results[0].pose.keypoints[9].score;
+        rightWrist_Score = results[0].pose.keypoints[10].score;
+        console.log("keypoints Score : " + leftWrist_Score + " , "+ rightWrist_Score);
+
+        X_leftWrist = results[0].pose.leftWrist.x;
+        Y_leftWrist = results[0].pose.leftWrist.y;
+        console.log("leftWrist : " + X_leftWrist +" , " + Y_leftWrist);
+
+        
+        X_rightWrist = results[0].pose.rightWrist.x;
+        Y_rightWrist = results[0].pose.rightWrist.y;
+        console.log("RightWrist : " + X_rightWrist +" , " + Y_rightWrist);
+
+    }
+
+
 }
 
 
